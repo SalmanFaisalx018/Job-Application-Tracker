5a08be295b88cfd1c18b38dc83fcc1b9e3d9a6a2
Salman FAISAL <salman.faisalx018@gmail.com>
2026-07-28 10:34:07 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const applicationRoutes = require("./routes/applicationRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
